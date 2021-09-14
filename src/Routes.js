dc3401002d19db7ff555fbcd62f7cd7eabbedd8c
(rhfovk) <(rhfovk@gmail.com)>
2021-09-14 09:28:06 +0900

    Add: config 사용 및 리팩토링 - Wip: 충돌 해결
    - Wip: 무비포스트 state 저장 및 통신 준비 중
    
    - Wip: 무비포스트 작성 로직 변경
    
    - Wip: 무비포스트 작성 로직 변경
    
    - Add: 컨벤션 정리

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import Main from './pages/Main/Main';
 import MoviePost from './pages/MoviePost/MoviePost';
 import Details from './pages/Details/Details';
 import Booking from './pages/Booking/Booking';
+import MoviePostWrite from './pages/MoviePostWrite/MoviePostWrite';
 
 function Routes() {
   return (
@@ -13,6 +14,7 @@ function Routes() {
       <Switch>
         <Route exact path="/" component={Main} />
         <Route exact path="/moviepost" component={MoviePost} />
+        <Route exact path="/moviepost/write" component={MoviePostWrite} />
         <Route exact path="/details" component={Details} />
         <Route exact path="/booking" component={Booking} />
       </Switch>
